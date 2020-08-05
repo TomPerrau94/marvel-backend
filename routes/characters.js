3556f039241101fc1356c2b8a4db56112b49e006
Tom Perrau <tomperrau@macbook-pro-tom.home>
2020-08-05 15:46:42 +0200

    added characterId specific comics route

--- routes/characters.js
+++ routes/characters.js
@@ -32,4 +32,31 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Récupérer tous les comics liés à un personnage
+router.get("/:id/comics", async (req, res) => {
+  try {
+    const apiPublic = process.env.MARVEL_API_PUBLIC;
+    const apiSecret = process.env.MARVEL_API_SECRET;
+
+    // Sécurisation de la clé api secret
+    // Création d'un timestamp
+    const date = new Date();
+    const ts = date.getTime() / 1000;
+
+    // Création du hash demandé par l'API Marvel
+    const hash = MD5(ts + apiSecret + apiPublic);
+
+    // Requête vers l'API Marvel
+    const response = await axios.get(
+      `https://gateway.marvel.com/v1/public/characters/${req.params.id}/comics?ts=${ts}&apikey=${apiPublic}&hash=${hash}`
+    );
+    console.log(response.data.status);
+
+    // Réponse au client
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
