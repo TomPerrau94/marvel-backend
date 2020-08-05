64afbf338e892edd3808053fac998ba55c1b55f9
Tom Perrau <tomperrau@macbook-pro-tom.home>
2020-08-05 14:30:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const formidable = require("express-formidable");
+// const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(formidable());
+
+// Import des routes
+const charactersRoutes = require("./routes/characters");
+// const comicsRoutes = require("./routes/comics");
+// const favoritesRoutes = require("./routes/favorites");
+
+// Initialisation des routes
+app.use(charactersRoutes);
+// app.use(comicsRoutes);
+// app.use(favoritesRoutes);
+
+// Lancement du serveur
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port : ${process.env.PORT}`);
+});
---
+++ routes/characters.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+// Import de crypto-js pour la sécurisation de la requête vers l'API Marvel
+const MD5 = require("crypto-js/md5");
+
+// Récupérer l'ensemble des personnages présents dans l'API Marvel
+router.get("/", async (req, res) => {
+  try {
+    const apiPublic = process.env.MARVEL_API_PUBLIC;
+    const apiSecret = process.env.MARVEL_API_SECRET;
+
+    // Sécurisation de l'api secret
+    // Création d'un timestamp
+    const date = new Date();
+    const ts = date.getTime() / 1000;
+
+    // Création du hash demandé par l'API Marvel
+    const hash = MD5(ts + apiSecret + apiPublic);
+
+    // Requête vers l'API Marvel
+    const response = await axios.get(
+      `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apiPublic}&hash=${hash}`
+    );
+    console.log(response.data);
+
+    // Réponse au client
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
