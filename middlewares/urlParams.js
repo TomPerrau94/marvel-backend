3adb04ba5c2312a89a3ad09d9165674f61839f8e
Tom Perrau <tomperrau@macbook-pro-tom.home>
2020-08-05 15:25:14 +0200

    added comics route

---
+++ middlewares/urlParams.js
@@ -0,0 +1,14 @@
+// const MD5 = require("crypto-js/md5");
+// const apiPublic = process.env.MARVEL_API_PUBLIC;
+// const apiSecret = process.env.MARVEL_API_SECRET;
+
+// const urlParams = () => {
+//   const date = new Date();
+//   const ts = date.getTime() / 1000;
+//   const hash = MD5(ts + apiSecret + apiPublic);
+
+//   const params = `ts=${ts}&apikey=${apiPublic}&hash=${hash}`;
+//   return params;
+// };
+
+// module.exports = urlParams;
