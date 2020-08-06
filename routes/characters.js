664e15d5c8a57537e2730be590b37bbeaea41200
Tom Perrau <tomperrau@macbook-pro-tom.home>
2020-08-06 12:20:17 +0200

    combined search routes in respective characters and comics routes

--- routes/characters.js
+++ routes/characters.js
@@ -59,4 +59,35 @@ router.get("/:id/comics", async (req, res) => {
   }
 });
 
+// Rechercher un personnage par nom
+router.get("/search/characters", async (req, res) => {
+  try {
+    const apiPublic = process.env.MARVEL_API_PUBLIC;
+    const apiSecret = process.env.MARVEL_API_SECRET;
+
+    // Sécurisation de l'api secret
+    // Création d'un timestamp
+    const date = new Date();
+    const ts = date.getTime() / 1000;
+
+    // Création du hash demandé par l'API Marvel
+    const hash = MD5(ts + apiSecret + apiPublic);
+
+    // Récupérer le nom entré dans la barre de recherche côté front
+    const nameSearched = req.query.nameStartsWith;
+    console.log(nameSearched);
+
+    // Requête vers l'API Marvel
+    const response = await axios.get(
+      `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${nameSearched}&ts=${ts}&apikey=${apiPublic}&hash=${hash}`
+    );
+    console.log(response.data.status);
+
+    // Réponse au client
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
--- routes/comics.js
+++ routes/comics.js
@@ -32,4 +32,36 @@ router.get("/comics", async (req, res) => {
   }
 });
 
+// Rechercher un comic par titre
+router.get("/search/comics", async (req, res) => {
+  try {
+    const apiPublic = process.env.MARVEL_API_PUBLIC;
+    const apiSecret = process.env.MARVEL_API_SECRET;
+
+    // Sécurisation de l'api secret
+    // Création d'un timestamp
+    const date = new Date();
+    const ts = date.getTime() / 1000;
+
+    // Création du hash demandé par l'API Marvel
+    const hash = MD5(ts + apiSecret + apiPublic);
+
+    // Récupérer le titre entré dans la barre de recherche côté front
+    console.log(req.query.titleStartsWith);
+    const titleSearched = req.query.titleStartsWith;
+    console.log(titleSearched);
+
+    // Requête vers l'API Marvel
+    const response = await axios.get(
+      `https://gateway.marvel.com/v1/public/comics?titleStartsWith=${titleSearched}&ts=${ts}&apikey=${apiPublic}&hash=${hash}`
+    );
+    console.log(response.data.status);
+
+    // Réponse au client
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
